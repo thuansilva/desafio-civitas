0517a607af3d1d65afa332ca46c549b75b444b01
Thuan Silva <thuansilva14@gmail.com>
2025-11-21 09:36:44 -0400

    refactor: rota para obter detalhes do monitoramento passando id

--- src/app/api/neighborhoods/[id]/route.ts
+++ src/app/api/neighborhoods/[id]/route.ts
@@ -1,57 +1,22 @@
-// import { FAKE_DETAILS_DATA } from "@/app/data-faker/faker";
-import { NextResponse } from "next/server";
+import { FAKE_READING } from "@/app/data-faker/faker-details-neighborhood";
+import { NextResponse, NextRequest } from "next/server";
 
-const FAKE_DETAILS_DATA = [
-  {
-    id: "1",
-    name: "Bairro A",
-    latitude: -22.9068,
-    longitude: -43.1729,
-    created_at: "2023-01-01T00:00:00Z",
-    latest_reading: {
-      id: "r1",
-      neighborhood_id: "1",
-      aqi: 42,
-      pm10: 12,
-      co: 0.4,
-      quality_level: "bom",
-      recorded_at: "2023-10-01T12:00:00Z",
-      created_at: "2023-10-01T12:05:00Z",
-    },
-  },
-  {
-    id: "2",
-    name: "Bairro B",
-    latitude: -22.9021,
-    longitude: -43.2103,
-    created_at: "2023-01-05T00:00:00Z",
-    latest_reading: null,
-  },
-];
-
-interface GetParams {
-  params: {
-    id: string[];
-  };
+interface RouteContext {
+  params: Promise<{ id: string }>;
 }
 
-export async function GET(request: Request, { params }: GetParams) {
-  // Pega o primeiro elemento do array 'id' ou undefined se o array estiver vazio
-  const neighborhoodId = params.id?.[0];
-  console.log(neighborhoodId);
+export async function GET(request: NextRequest, context: RouteContext) {
+  const { id } = await context.params;
 
-  if (neighborhoodId) {
-    const neighborhood = FAKE_DETAILS_DATA.find((n) => n.id === neighborhoodId);
-
-    if (!neighborhood) {
-      // Se o ID foi fornecido mas o bairro não existe
-      return NextResponse.json(
-        { error: `Bairro com ID ${neighborhoodId} não encontrado` },
-        { status: 404 },
-      );
-    }
-    return NextResponse.json(neighborhood);
-  } else {
-    return NextResponse.json(FAKE_DETAILS_DATA);
+  const neighborhood = FAKE_READING.find((n) => n.id === id);
+  if (!neighborhood) {
+    return NextResponse.json(
+      { error: `Bairro com ID ${neighborhood} não encontrado` },
+      { status: 404 },
+    );
   }
+
+  const latestReading = neighborhood.latest_reading;
+
+  return NextResponse.json(latestReading);
 }
